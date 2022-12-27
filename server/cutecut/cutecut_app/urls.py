87a17ce562aa336278c44b34cff1515bdc873e69
Yazidechitou <yazidechitou@gmail.com>
2022-12-27 08:08:21 -0600

    favorites and Newsletter features

--- server/cutecut/cutecut_app/urls.py
+++ server/cutecut/cutecut_app/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('api/cryptolist',views.cryptolist),
+    path('api/cryptolist/',views.cryptolist),
+    path('api/cryptonews/<str:category>',views.cryptonews),
     path('signin/',views.signIn),
     path('signup/',views.signUp),
     path('signout/',views.signOut),
-    path('curr_user', views.curr_user),
+    path('current_user/', views.current_user),
     re_path('.*/',views.index)
 ]
--- server/cutecut/cutecut_app/views.py
+++ server/cutecut/cutecut_app/views.py
@@ -6,27 +6,15 @@ from django.contrib.auth.decorators import login_required
 from .models import *
 from django.core.serializers import serialize
 import json , requests
-from django.views.decorators.csrf import csrf_protect
+import http.client
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
 
-# Create your views here.
 
 def index (request):
     homePage = open('static/index.html').read()
     return HttpResponse(homePage)
 
-@api_view(["GET"])
-def cryptolist (request):
-    response = requests.get("https://api.coingecko.com/api/v3/coins/list?include_platform=true")
-    response= response.json()
-    # print(response)
-    # response_json = serialize("json",response)
-    # response_json = json.loads(response_json)
-    return JsonResponse(response, safe=False)
-
-    
-
 @csrf_protect
-
 @api_view(["POST"])
 def signIn(request):
     print("this is request in Sign In", request, request.data)
@@ -36,10 +24,10 @@ def signIn(request):
     if user.is_active:
         try:
             login(request._request, user)
-            return JsonResponse({"Success SignIn":True})
+            return JsonResponse({"SignIn":True})
         except:
-            return JsonResponse({"Success SignIn":False})
-    return JsonResponse({"Success SignIn":False})
+            return JsonResponse({"SignIn":False})
+    return JsonResponse({"SignIn":False})
 
 @csrf_protect
 @api_view(["POST"])
@@ -50,9 +38,9 @@ def signUp(request):
         password=request.data["password"]
         print("User email and password are:",email, password)
         AppUser.objects.create_user(username=email,email=email,password=password)
-        return JsonResponse({"Success SignUp":True})
+        return JsonResponse({"SignUp":True})
     except:
-        return JsonResponse({"Success SignUp":False})
+        return JsonResponse({"SignUp":False})
 
 @api_view(["POST"])
 @login_required
@@ -60,12 +48,47 @@ def signOut(request):
     if user.is_authenticated():
         try:
             logout(request._request)
-            return JsonResponse({"Success SignOut":True})
+            return JsonResponse({"SignOut":True})
         except :
-            return JsonResponse({"Success SignOut": False})
-    return JsonResponse({"Success SignOut": False})
+            return JsonResponse({"SignOut": False})
+    return JsonResponse({"SignOut": False})
 
-def curr_user (request):
-    pass
+@api_view(["GET"])
+def current_user (request):
+    if request.user.is_authenticated:
+        data = serialize("json", [request.user], fields=["email"])
+        print(data)
+        return JsonResponse(data, safe=False)
+    return JsonResponse({"user":None})
 
+
+@api_view(["GET"])
+def cryptolist (request):
+    conn = http.client.HTTPSConnection("coinranking1.p.rapidapi.com")
+    headers = {
+    'X-RapidAPI-Key': "c6b134ca9amshb4c2b50e15d6438p153f0bjsnd635775334a8",
+    'X-RapidAPI-Host': "coinranking1.p.rapidapi.com"
+    }
+    conn.request("GET", "/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=500&offset=0", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    data= data.decode("utf-8")
+    data_json = data.replace("'", '"')
+    data_json = json.loads(data_json)
+    return JsonResponse(data_json, safe=False)
+
+
+@api_view(["GET"])
+def cryptonews (request,category):
+    print("Request of crypto news", category)
+    url = "https://bing-news-search1.p.rapidapi.com/news/search"
+    querystring = {"q":category,"safeSearch":"Off", "textFormat":"Raw","freshness":"Day","count":"100"}
+    headers = {
+        "X-BingApis-SDK": "true",
+        "X-RapidAPI-Key": "c6b134ca9amshb4c2b50e15d6438p153f0bjsnd635775334a8",
+        "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = response.json()
+    return JsonResponse(response, safe=False)
 
